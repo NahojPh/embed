b51ae4be232ca585258c4d22e7e3094a37530024
Johan Philip <johan.philip@skola.malung-salen.se>
2021-11-25 15:48:04 +0100

    Added carService and three characteristics. Started to advertise the service and set a local name. Compiles.

--- bildriver/bildriver.ino
+++ bildriver/bildriver.ino
@@ -1,9 +1,33 @@
+#include <ArduinoBLE.h>
+
+BLEService carService("180F");
+
+
+BLECharacteristic carInfoChar("2A19", BLERead, 12, sizeof(12));
+BLECharCharacteristic carSteerChar("2A20", BLEWrite);
+BLECharCharacteristic carDriveChar("2A21", BLEWrite);
+
+
+
+short OldDriveScale = 0; // 0 - 11 där 0 - 4 är bakåt, 5 är stilla och 6 - 11 är framåt.
+//char OldCarInfo[8] = ["0000000"];
+char oldCarInfo[] = { '0', '0', '0', '0', '0', '0', '0', '0', '\0' };
+
 void setup() {
-  // put your setup code here, to run once:
+  if (BLE.begin()) {
+      
+    }
+
+  BLE.setLocalName("Car-Captain");
+  BLE.setAdvertisedService(carService); //Add the service uuid
+
+  //Add characteristics
+  carService.addCharacteristic(carInfoChar);
+  carService.addCharacteristic(carSteerChar);
+  carService.addCharacteristic(carDriveChar);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
