8c2ce04e52eba876e7e0f0a48f1093c4c7357bcf
Johan Philip <johan.philip@skola.malung-salen.se>
2022-03-30 11:03:09 +0200

    Changed back once again to arduino because the raspberry pi does not want to send ström.

--- bildriver/upload_n_run.bat
+++
@@ -1,2 +0,0 @@
-scp C:\Users\NahojPh\Desktop\embed\bildriver\target\armv7-unknown-linux-gnueabihf\debug\bildriver ubuntu@192.168.1.123:/home/ubuntu
-ssh ubuntu@192.168.1.123 chmod +x ~/bildriver && ~/bildriver
