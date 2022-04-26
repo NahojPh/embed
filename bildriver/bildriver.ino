f7245d3a0a7c581dee220307ffa2b6be37f04d39
NahojPh <johan.philip04@gmail.com>
2022-04-26 20:04:10 +0200

    Added repeated polling into the void update method to recv incoming ble events. Created a BLEWrite handle so write events have a callback. Fixed the formatting on the char data which is now much simpler.

--- bildriver/bildriver.ino
+++ bildriver/bildriver.ino
@@ -5,26 +5,42 @@ const char* deviceServiceCharacteristicUuid = "19b10001-e8f2-537e-4f6c-d104768a1
 
 
 void setup() {
-  char driveData[16] = "123456781234567";
+  //Having two values each for one side of the vehicle where the middle of the
+  //255 value is where is changes the motor for that side so it can change direction.
+  char driveData[8] = "255:255"; 
+                                
   
   BLEService driveService(deviceServiceUuid); 
-  BLECharacteristic driveCharacteristic(deviceServiceCharacteristicUuid, BLERead | BLEWriteWithoutResponse, driveData);
+  //We only want to read and write without response because the client (phone) is the one who is going to control the speed.
+  BLECharacteristic driveCharacteristic(deviceServiceCharacteristicUuid, BLERead | BLEWriteWithoutResponse, driveData); 
 
   if (!BLE.begin()) {
-    serial.println("Could not start bt");
+    Serial.println("Could not start bt");
     while(1);
   }
   BLE.setLocalName("Bildriver");
   BLE.setAdvertisedService(driveService);
-  gestureService.addCharacteristic(driveCharacteristic);
+  Serial.println("Starting to advertise");
+  driveService.addCharacteristic(driveCharacteristic);
+  Serial.println("Added characteristic");
   BLE.addService(driveService);    
-  gestureCharacteristic.writeValue("123456781234567");
+  Serial.println("Added the service");
+  driveCharacteristic.setValue("0:0");
+  Serial.println("Set inital data for the driveCharacteristic");
+
+  driveCharacteristic.setEventHandler(BLEWritten, driveValueWritten);
 
   BLE.advertise();
 
 }
 
 void loop() {
-  
+  //Polls for ble events :)
+  BLE.poll();
 
+}
+
+void driveValueWritten(BLEDevice central, BLECharacteristic car) {
+  Serial.println(*car.value());
+  
 }
