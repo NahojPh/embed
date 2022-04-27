7b9c948718e06686478bd043dc1330f0ef3d06a7
Johan Philip <johan.philip@skola.malung-salen.se>
2022-04-27 11:54:52 +0200

    Made the Service and char uuid different. Now the driveChar has an internal buffer with a size (of 7). Ca now read the value from a write event.

--- bildriver/bildriver.ino
+++ bildriver/bildriver.ino
@@ -1,18 +1,17 @@
 #include <ArduinoBLE.h>
 
-const char* deviceServiceUuid = "19b10000-e8f2-537e-4f6c-d104768a1214";
-const char* deviceServiceCharacteristicUuid = "19b10001-e8f2-537e-4f6c-d104768a1214";
+//The diff between the device deviceServiceUuid and the deviceServiceCharacteristicUuid is the 4 and 5 in the end.
+const char* deviceServiceUuid = "19b10000-e8f2-537e-4f6c-d104768a1214"; 
+const char* deviceServiceCharacteristicUuid = "19b10001-e8f2-537e-4f6c-d104768a1215";
 
 
 void setup() {
   //Having two values each for one side of the vehicle where the middle of the
   //255 value is where is changes the motor for that side so it can change direction.
-  char driveData[8] = "255:255"; 
-                                
   
   BLEService driveService(deviceServiceUuid); 
   //We only want to read and write without response because the client (phone) is the one who is going to control the speed.
-  BLECharacteristic driveCharacteristic(deviceServiceCharacteristicUuid, BLERead | BLEWriteWithoutResponse, driveData); 
+  BLECharacteristic driveCharacteristic(deviceServiceCharacteristicUuid, BLERead | BLEWriteWithoutResponse, 7, false); 
 
   if (!BLE.begin()) {
     Serial.println("Could not start bt");
@@ -25,9 +24,9 @@ void setup() {
   Serial.println("Added characteristic");
   BLE.addService(driveService);    
   Serial.println("Added the service");
-  driveCharacteristic.setValue("0:0");
+  //driveCharacteristic.setValue("");
   Serial.println("Set inital data for the driveCharacteristic");
-
+  driveCharacteristic.setValue("0:0");
   driveCharacteristic.setEventHandler(BLEWritten, driveValueWritten);
 
   BLE.advertise();
@@ -41,6 +40,22 @@ void loop() {
 }
 
 void driveValueWritten(BLEDevice central, BLECharacteristic car) {
-  Serial.println(*car.value());
+  
+  char buf[7];
+  car.readValue(buf, 7);
+  /*
+   * https://www.best-microcontroller-projects.com/arduino-strtok.html
+  char str*;
+  char *p = buf;
+
+  while ((str = strtok(p, ":")) != "\n"){
+    Serial.println(str);
+    p = NULL;
+  }
+  */
+  //String str = String(buf);
+  //Serial.print(str);
+
+  
   
 }
