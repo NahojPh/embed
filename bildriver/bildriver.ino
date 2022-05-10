1099c0602208dd5193c27573a1178c010bbc4a17
NahojPh <44095441+NahojPh@users.noreply.github.com>
2022-05-10 23:06:10 +0200

    Update bildriver.ino
    Fixed merge conflict and readded the real splitter and converter.

--- bildriver/bildriver.ino
+++ bildriver/bildriver.ino
@@ -1,3 +1,4 @@
+
 #include <ArduinoBLE.h>
 
 //The diff between the device deviceServiceUuid and the deviceServiceCharacteristicUuid is the 4 and 5 in the end.
@@ -11,7 +12,7 @@ void setup() {
   
   BLEService driveService(deviceServiceUuid); 
   //We only want to read and write without response because the client (phone) is the one who is going to control the speed.
-  BLECharacteristic driveCharacteristic(deviceServiceCharacteristicUuid, BLERead | BLEWriteWithoutResponse, 7, false); 
+  BLECharacteristic driveCharacteristic(deviceServiceCharacteristicUuid, BLERead | BLEWrite | BLEWriteWithoutResponse, 7, false); 
 
   if (!BLE.begin()) {
     Serial.println("Could not start bt");
@@ -43,56 +44,33 @@ void driveValueWritten(BLEDevice central, BLECharacteristic car) {
   
   char buf[8];
   car.readValue(buf, 7);
-  buf[sizeof(buf)/sizeof(char)] = 0;
+
 
 
   // https://arduino.stackexchange.com/questions/1013/how-do-i-split-an-incoming-string
-  // Read each command pair 
-  char* command = strtok(buf, ":");
-  while (command != 0)
-  {
-      // Split the command in two values
-      char* separator = strchr(command, ':');
-      if (separator != 0)
-      {
-          // Actually split the string in 2: replace ':' with 0
-          *separator = 0;
-          int driveSpeed = atoi(command);
-          ++separator;
-          int steerCont = atoi(separator);
-
-          Serial.println(driveSpeed);
-          Serial.println(steerCont);
-        
-      }
-  }
-  
-  
-  //Buffers for the L-lane and R-lane.
- // char lBuf[3];
-//  char rBuf[3];
-  
-//  char divider = ':';
-//  int bufSize = sizeof(buf)/sizeof(char);
-
-//  strtok();
-
-//  int driveSpeed = bufferToInt(lBuf);
-//  int steerCont = bufferToInt(rBuf);
-  //Docs: https://www.arduino.cc/reference/en/language/functions/analog-io/analogwrite/
-  //If we have drager the (phone) slider to the left then one pwm gets activated.
-
-  
-}
 
 
-int bufferToInt(char* buff) {
-    int sum = 0;
-    //Use the index of the value with scientific notation to create an interger.
-    int buffSize = sizeof(buff)/sizeof(char);
+    // Split the command in two values
+    char* separator = strchr(buf, ':');
+    if (separator != 0)
+    {
+        // Actually split the string in 2: replace ':' with 0
+        *separator = 0;
+        int driveSpeed = atoi(buf);
+        ++separator;
+        int steerCont = atoi(separator);
 
-    for (int i = 0; i < buffSize; i++) {
-        sum += (buff[i] * pow(10, buffSize - (i+1)));
+        Serial.println(driveSpeed);
+        Serial.println(steerCont);
+
+
+
+        if (steerCont > 127) {
+          //Analog write this incase its right
+        }
+        else {
+          //Else analog write this if its left
+        }
     }
-    return sum;
+  
 }
